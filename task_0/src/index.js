90d5ef275ec8e7cbede3ddbaf665dcce8ecfb63f
wahujos <jwahu30@gmail.com>
2025-04-18 17:32:36 +0300

    Basic setup

---
+++ task_0/src/index.js
@@ -0,0 +1,8 @@
+import $ from "jquery";
+
+$(document).ready(() => {
+    $("body").append("<p>ALX Dashboard</p>");
+    $("body").append("<p>Dashboard data for the students</p>");
+    $("body").append("<p>Copyright - ALX</p>");
+
+});
